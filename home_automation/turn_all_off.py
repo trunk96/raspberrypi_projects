87236375bc2a54eb532a213f2ca1e5c752dacb9e
Emanuele De Santis <trunk96@gmail.com>
2020-03-12 10:33:55 +0100

    first commit

---
+++ home_automation/turn_all_off.py
@@ -0,0 +1,32 @@
+from zeroconf import ServiceBrowser, Zeroconf
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+shelly_list={}
+
+class shelly_listener:
+    def remove_service(self, zeroconf, type, name):
+        name = name.split('.')[0]
+        if name in shelly_list:
+            del(shelly_list[name])
+
+    def add_service(self, zeroconf, type, name):
+        if ("shelly" in name) and (name not in shelly_list):
+            name = name.split('.')[0]
+            shelly_list[name] = name+".local"
+
+@app.route("/turn_off")
+def turn_off_shelly():
+    payload = {'turn': 'off'}
+    for shelly in shelly_list:
+        r = requests.post("http://"+shelly_list[shelly]+"/relay/0", data = payload)
+
+
+if __name__ == "__main__":
+    zeroconf = Zeroconf()
+    listener = shelly_listener()
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+    app.run(host = '0.0.0.0', port=80)
