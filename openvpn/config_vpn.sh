fdfe315791279dd7f364057b7dae0bb2524b2eea
Emanuele De Santis <trunk96@gmail.com>
2021-12-15 21:11:37 +0100

    Updated dyndns address

--- openvpn/config_vpn.sh
+++ openvpn/config_vpn.sh
@@ -2,5 +2,5 @@ git clone https://github.com/kylemanna/docker-openvpn.git docker-openvpn
 OVPN_DATA="ovpn-data-casa"
 docker volume create --name $OVPN_DATA
 docker build -t openvpn-server:latest ./docker-openvpn
-docker run -v $OVPN_DATA:/etc/openvpn --rm openvpn-server ovpn_genconfig -u udp://VPN.SERVERNAME.COM
+docker run -v $OVPN_DATA:/etc/openvpn --rm openvpn-server ovpn_genconfig -u udp://desantisemanuele.ddns.net
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it openvpn-server ovpn_initpki
